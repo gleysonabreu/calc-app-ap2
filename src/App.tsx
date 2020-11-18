f39ee7a3377feb99c7293dd399c69c90a7775881
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-11-18 09:17:48 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Container, Wrapper, Calculator,
+  CalcValue, ComponentsCalc, Button,
+  NumbersCalc, Numbers, ButtonsNumber, ButtonEqual
+} from './css/app';
 
 function App() {
+
+  const [infoScreen, setInfoScreen] = useState('');
+
+  const [operator, setOperator] = useState('');
+  const [calc, setCalc] = useState<number>();
+
+  function addOperator(op: string){
+    setOperator(op);
+    setInfoScreen('');
+  }
+
+  function addNumbersAndOperator(info: string){
+    setInfoScreen(infoScreen + info);
+  }
+
+  function result(){
+    console.log(infoScreen);
+    const number = parseFloat(parseFloat(infoScreen).toFixed(2));
+    calculator(operator, number);
+    setInfoScreen(String(calc));
+  }
+
+  function calculator(op: string, number: number){
+    switch(op){
+      case '+':
+        setCalc(calc + number);
+        break;
+      case '-':
+        setCalc(calc - number);
+        break;
+      case '*':
+        setCalc(calc * number);
+        break;
+      case '/':
+        setCalc(calc / number);
+        break;
+    }
+  }
+  
+  
   return (
-    <div className="App">
-      Hello world.
-    </div>
+    <Container>
+      <Wrapper>
+        <Calculator>
+          <CalcValue><span>{infoScreen}</span></CalcValue>
+          <ComponentsCalc>
+            <Button onClick={() => addOperator('+')}>+</Button>
+            <Button onClick={() => addOperator('-')}>-</Button>
+            <Button onClick={() => addOperator('/')}>/</Button>
+            <Button onClick={() => addOperator('*')}>*</Button>
+          </ComponentsCalc>
+          <NumbersCalc>
+            <Numbers>
+              <ButtonsNumber onClick={() => addNumbersAndOperator('9')}>9</ButtonsNumber>
+              <ButtonsNumber onClick={() => addNumbersAndOperator('8')}>8</ButtonsNumber>
+              <ButtonsNumber onClick={() => addNumbersAndOperator('7')}>7</ButtonsNumber>
+              <ButtonsNumber onClick={() => addNumbersAndOperator('6')}>6</ButtonsNumber>
+              <ButtonsNumber onClick={() => addNumbersAndOperator('5')}>5</ButtonsNumber>
+              <ButtonsNumber onClick={() => addNumbersAndOperator('4')}>4</ButtonsNumber>
+              <ButtonsNumber onClick={() => addNumbersAndOperator('3')}>3</ButtonsNumber>
+              <ButtonsNumber onClick={() => addNumbersAndOperator('2')}>2</ButtonsNumber>
+              <ButtonsNumber onClick={() => addNumbersAndOperator('1')}>1</ButtonsNumber>
+              <ButtonsNumber onClick={() => addNumbersAndOperator('0')}>0</ButtonsNumber>
+              <ButtonsNumber onClick={() => addNumbersAndOperator('.')}>.</ButtonsNumber>
+              <ButtonsNumber>C</ButtonsNumber>
+            </Numbers>
+            <ButtonEqual onClick={result}>=</ButtonEqual>
+          </NumbersCalc>
+        </Calculator>
+      </Wrapper>
+    </Container>
   );
 }
 
---
+++ src/css/app.ts
@@ -0,0 +1,122 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  color: #333;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 30px;
+`;
+
+export const Wrapper = styled.div`
+  width: 100%;
+  background: #d2d2d2;
+  padding: 20px;
+  border-radius: 15px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media (min-width: 1100px) {
+    max-width: 800px;
+    max-height: 600px;
+  }
+`;
+
+export const Calculator = styled.div`
+  width: 400px;
+  
+  border-radius: 15px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  border-radius: 15px;
+`;
+
+export const CalcValue = styled.div`
+  width: 100%;
+  padding: 5px;
+  font-size: 22px;
+  color: #fff;
+
+  display: flex;
+
+  background: #333;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+
+  span{
+    text-align: right;
+    width: 100%;
+  }
+`;
+
+export const ComponentsCalc = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(4, 100px);
+  grid-template-areas: "soma subtracao divisao multiplicacao";
+
+  button:nth-child(0){
+    grid-area: soma;
+  }
+  button:nth-child(1){
+    grid-area: subtracao;
+  }
+
+  button:nth-child(2){
+    grid-area: divisao;
+  }
+
+  button:nth-child(3){
+    grid-area: multiplicacao;
+  }
+`;
+
+export const Button = styled.button`
+  border-radius: 0px;
+  border: 0;
+  padding: 10px;
+  outline: none;
+  background-color: #DEBA6F;
+  font-size: 19px;
+  cursor: pointer;
+  transition: background-color .2s;
+
+  &:hover{
+    background-color: #E5C88B;
+  }
+`;
+
+export const NumbersCalc = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`;
+
+export const Numbers = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 90px);
+`;
+
+export const ButtonsNumber = styled(Button)`
+  background: #b5b5b5;
+
+  &:nth-child(9){
+    border-bottom-left-radius: 16px;
+  }
+  &:hover{
+    background-color: #cecece;
+  }
+`;
+
+export const ButtonEqual = styled(Button)`
+  width: 100%;
+
+  border-bottom-right-radius: 16px;
+`;
